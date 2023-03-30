020562bc97c03984881f457cddc5ed656d90e602
Isac Petinate <idpetinate@gmail.com>
2023-03-30 08:44:05 -0300

    Reestructuring App module

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
+
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import './App.css'
+import './App.styles.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
+    <div className='app'>
       <div>
         <a
           href='https://vitejs.dev'
@@ -51,5 +52,3 @@ function App() {
     </div>
   )
 }
-
-export default App
