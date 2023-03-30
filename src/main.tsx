ebe1695a1ae9601469406323dfe670371896991b
Isac Petinate <idpetinate@gmail.com>
2023-03-30 08:28:07 -0300

    Lint files

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ import './styles/tailwind.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
